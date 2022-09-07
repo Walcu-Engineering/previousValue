4ad2b6afc41a75622b2b89ebae8e042cad7d499b
Jose G.L <jgl@mailbox.org>
2022-09-07 18:52:50 +0200

    Add test suit

--- index.test.js
+++ index.test.js
@@ -1,32 +1,29 @@
-const previousValue = require('./lib/previousValue.js'); 
+const previousValue = require('./lib/getPreviousValue.js'); 
 
-beforeAll(() => {
-  const changed_customer = {
-    name: 'Test name',
-    surname: 'Test surname',
-    theundefined: 'some value',
-    contacts: [{
-      name: 'Test contact 1 name',
-      phones: ['Test contact 1 phone 1', 'Test contact 1 phone 2'],
-      emails: ['Test contact 1 email 1', 'Test contact 1 email 2'],
-    }],
-  };
-  const diffs1 = [
-    {path: '/theundefined'}, //old value was not defined.
-    {path: '/contacts/0', old_value: {name: 'old name', phones: ['old phone 1', 'old phone 2'], emails: ['old email 1', 'old email 2']}},
-  ]
-  const undone_customer = {
-    name: 'old name',
-    surname: 'Test surname',
-    contacts: [{
-      name: 'old contact name',
-      phones: ['old phone 1', 'old phone 2'],
-      emails: ['old email 1', 'old email 2'],
-    }],
-  }
-  return 0;
-})
-
+const changed_customer = {
+  name: 'Test name',
+  surname: 'Test surname',
+  theundefined: 'some value',
+  contacts: [{
+    name: 'Test contact 1 name',
+    phones: ['Test contact 1 phone 1', 'Test contact 1 phone 2'],
+    emails: ['Test contact 1 email 1', 'Test contact 1 email 2'],
+  }],
+};
+const diffs = [
+  {path: '/theundefined'}, //old value was not defined.
+  {path: '/name', old_value: 'old name'},
+  {path: '/contacts/0', old_value: {name: 'old contact name', phones: ['old phone 1', 'old phone 2'], emails: ['old email 1', 'old email 2']}},
+]
+const undone_customer = {
+  name: 'old name',
+  surname: 'Test surname',
+  contacts: [{
+    name: 'old contact name',
+    phones: ['old phone 1', 'old phone 2'],
+    emails: ['old email 1', 'old email 2'],
+  }],
+}
 
 describe('previousValue', () => {
   test('/theundefined previous value should be undefined', () => {
@@ -49,10 +46,10 @@ describe('previousValue', () => {
     const fully_restored = previousValue(changed_customer, diffs, '');
     expect(fully_restored).toEqual(undone_customer);
   });
-  test('Restored without changes. Should return undefined', () => {
-    expect(previousValue(changed_customer, [], '')).toBe(undefined);
+  test('Restored without changes. Should return the same object', () => {
+    expect(previousValue(changed_customer, [], '')).toEqual(changed_customer);
   });
   test('Prev deep path value without changes. Should return undefined', () => {
-    expect(previousValue({undone_customer, diffs, '/deep/path')).toBe(undefined);
+    expect(previousValue(undone_customer, diffs, '/deep/path')).toBe(undefined);
   });
 });
